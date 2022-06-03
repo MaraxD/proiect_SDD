b627228674296806cd50572b79249fa82cfb5ce0
Mara Olaru <olarumara20@stud.ase.ro>
2022-06-03 19:28:16 +0300

    also did the functions the binary tree

--- Source.c
+++ Source.c
@@ -19,6 +19,7 @@ typedef struct Tranzit Tranzit;
 typedef struct NodPrincipal NodPrincipal;
 typedef struct NodSecundar NodSecundar;
 typedef struct Nod Nod;
+typedef struct NodA NodA;  //pentru arbori
 typedef struct ListaD ListaD;
 
 enum TIP {
@@ -50,6 +51,12 @@ struct Nod {
 	Tranzit info;
 	Nod* next;
 	Nod* prev;
+};
+
+struct NodA {
+	Tranzit info;
+	Nod* dreapta;
+	Nod* stanga;
 };
 
 struct ListaD {
@@ -93,10 +100,11 @@ Tranzit creareTransit(FILE* file)
 
 }
 
+//-------------functiile pentru grafuri------------------------
 void inserareListaPrincipala(NodPrincipal** cap, Tranzit tr) {
 	NodPrincipal* nou = (NodPrincipal*)malloc(sizeof(NodPrincipal));
-	nou->info = tr;
 	nou->next = NULL;
+	nou->info = tr;
 	nou->vecini = NULL;
 	NodPrincipal* aux = *cap;
 	if (*cap) {
@@ -112,10 +120,10 @@ void inserareListaPrincipala(NodPrincipal** cap, Tranzit tr) {
 
 void inserareListaSecundara(NodSecundar** cap, NodPrincipal* infoT) {
 	NodSecundar* nou = (NodSecundar*)malloc(sizeof(NodSecundar));
-	nou->info = infoT;
 	nou->next = NULL;
+	nou->info = infoT;
+	NodSecundar* aux = *cap;
 	if (*cap) {
-		NodSecundar* aux = *cap;
 		while (aux->next) {
 			aux = aux->next;
 		}
@@ -182,15 +190,14 @@ void inserareVecini(NodPrincipal* nod, int id1, int id2) {
 	}
 }
 
-
 Tranzit creareDefault() {
 	Tranzit t;
-	t.id = NULL;
+	t.id = -1;
 	t.tip = -1;
 	t.lungime = 00.0;
 	t.nrMedCalatori = -1;
 	t.nrStatii = -1;
-	t.denumire = NULL;
+	t.denumire = "-";
 	t.anDStatii = -1;
 
 	return t;
@@ -211,13 +218,112 @@ void stergereID(NodPrincipal** nod, int id) {
 		stergereID((*nod)->next, id);
 	}
 	(*nod) = aux;
+}
+
+//b)Stergerea si dezalocarea grafului(toate nodurile);
+void dezalocare(NodPrincipal* cap) {
+	while (cap) { //daca am un cap in lista principala inseamna ca am 0 sau mai multi vecini
+		while (cap->vecini) { //verific daca am un urmator, pentru ca atunci ar trebui sa ma mut pe urm poz inainte sa dezaloc
+			free(cap->vecini->info.denumire);
+			free(cap->vecini->info.anDStatii);
+			NodSecundar* aux1 = cap->vecini;
+			cap->vecini = cap->vecini->next;
+			free(aux1);
+		}
+		free(cap->info.denumire);
+		NodPrincipal* aux2 = cap;
+		cap = cap->next;//ma duc pe urm nod din lista principala si dupa dezaloc
+		free(aux2);
+	}
+
+	//cap = NULL;
+}
+
+//void dezalocareLP(NodPrincipal** cap) {
+//	NodPrincipal* aux = (*cap);
+//	while (aux->next) {
+//		NodPrincipal* aux2 = aux->next;
+//		if (aux->vecini)
+//			dezalocareLS(aux->vecini);
+//		//printf("\nAm sters %s", aux->info.denumire);
+//		free(aux->info.denumire);
+//		free(aux);
+//		aux = aux2;
+//	}
+//	(*cap) = NULL;
+//}
 
 
+
+//----------------------functiile pentru arbore binar------------
+void inserareArbore(NodA** rad, Tranzit tr) {
+	if ((*rad) == NULL) {
+		NodA* nou = (NodA*)malloc(sizeof(NodA));
+		nou->info = tr;
+		nou->dreapta = nou->stanga = NULL;
+		(*rad) = nou;
+	}
+	else {
+		if (tr.id > (*rad)->info.id) {
+			inserareArbore(&(*rad)->dreapta, tr);
+		}
+		else {
+			inserareArbore(&(*rad)->stanga, tr);
+		}
+	}
+}
+
+void parcurgereInOrdine(NodA* rad) {
+	if (rad) {
+		parcurgereInOrdine(rad->stanga);
+		afisareTranzit(rad->info);
+		parcurgereInOrdine(rad->dreapta);
+	}
 
 }
 
+//face cautarea si in subarbori
+void cautare(NodA** curent, int id, NodA** parinte) {
+	while (curent != NULL && (*curent)->info.id != id) {
+		parinte = curent;
 
-//functiile pentru lista dubla
+		if (id < (*curent)->info.id) {
+			(*curent) = (*curent)->stanga;
+		}
+		else {
+			(*curent) = (*curent)->dreapta;
+		}
+	}
+
+}
+
+int nrCopii(NodA* rad) {
+	if (rad) {
+		int cdr = nrCopii(rad->dreapta);
+		int cstg = nrCopii(rad->stanga);
+		return cstg - cdr;
+	}
+	return 0;
+}
+
+//c)Stergerea unui nod din arbore dupa id;
+//daca nodul cautat este chiar frunza, acesta se sterge normal
+//daca nodul are un copil, acesta este inlocuit de copil
+//daca nodul are doi copii, acesta este inlocuit de copilul cu valoarea cea mai mare (compari id urile)
+//nr de copii este dat de diferenta de inaltime
+void stergereInArbore(NodA** ndr, int id) {
+	if ((*ndr)->dreapta && (*ndr)->dreapta->info.id==id) {
+		stergereInArbore((*ndr)->dreapta, id);
+	}
+	else {
+
+
+	}
+}
+
+
+
+//----------------------functiile pentru lista dubla-----------------------
 //inserare la inceput
 void inserareListaD(ListaD* cap, Tranzit tr) {
 	Nod* nou = (Nod*)malloc(sizeof(Nod));
@@ -246,13 +352,13 @@ void stergereNod(ListaD* cap, char* denumire) {
 	Nod* aux = cap->prim;
 	Nod* aux2 = cap->ultim;
 	if (cap->prim || cap->ultim) {
-		if (strcmp(cap->prim->info.denumire, denumire)==0) { //daca intra inseamna ca exista si primul cap/ultimul cap
+		if (strcmp(cap->prim->info.denumire, denumire) == 0) { //daca intra inseamna ca exista si primul cap/ultimul cap
 			cap->prim = aux->next; //urmatorul nod de dupa nodul sters va deveni noul cap
 			free(aux->info.denumire); //stergi capul (unde se afla elem cautat)
 			free(aux);
 		}
 		//daca se afla la sfarsit
-		else if (strcmp(cap->ultim->info.denumire, denumire)==0) {
+		else if (strcmp(cap->ultim->info.denumire, denumire) == 0) {
 			cap->ultim = aux2->prev;
 			free(aux2->info.denumire); //stergi capul (unde se afla elem cautat)
 			free(aux2);
@@ -261,14 +367,14 @@ void stergereNod(ListaD* cap, char* denumire) {
 		else {
 			aux = aux->next;
 			/*while (aux->next) {*/
-				if (strcmp(aux->info.denumire, denumire)==0) {
-					aux->prev->next = aux->next;
-					free(aux->info.denumire);
-					free(aux);
-				}/*
-				else {
-					aux = aux->next;
-				}*/
+			if (strcmp(aux->info.denumire, denumire) == 0) {
+				aux->prev->next = aux->next;
+				free(aux->info.denumire);
+				free(aux);
+			}/*
+			else {
+				aux = aux->next;
+			}*/
 
 			/*}*/
 		}
@@ -290,25 +396,33 @@ void main() {
 	f = fopen("tranzits.txt", "r");
 
 	//arbore
-	/*inserareListaPrincipala(&cap, creareTransit(f));
+	inserareListaPrincipala(&cap, creareTransit(f));
 	inserareListaPrincipala(&cap, creareTransit(f));
 	inserareListaPrincipala(&cap, creareTransit(f));
 	inserareVecini(cap, 1, 2);
 	inserareVecini(cap, 1, 3);
 	afisareLista(cap);
-	stergereID(&cap, 2);
+
+	//a)
+	/*stergereID(&cap, 2);
 	printf("\n\ngraf dupa sterge nod\n\n");
 	afisareLista(cap);*/
 
+
+	//b)
+	dezalocare(cap);
+	printf("\n\ngraf dupa dezalocare\n\n");
+	afisareLista(cap);
+
 	//lista dubla
-	inserareListaD(&listad, creareTransit(f));
+	/*inserareListaD(&listad, creareTransit(f));
 	inserareListaD(&listad, creareTransit(f));
 	inserareListaD(&listad, creareTransit(f));
 
 	afisareLD(listad);
 	stergereNod(&listad, "Bakerloo line");
 	printf("\n\n");
-	afisareLD(listad);
+	afisareLD(listad);*/
 
 
 
